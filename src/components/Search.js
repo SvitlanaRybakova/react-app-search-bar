e12da2653ec8d9b14c9d50aba8d5d5abda54bf0e
Svitlana <rsv0011@gmail.com>
2021-02-17 17:28:10 +0100

    create Search component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+
+function Search(){
+  return(
+    <form action="">
+      <input type="text" placeholder="search"/>
+      <button>Find</button>
+    </form>
+  )
+}
+export default Search;
