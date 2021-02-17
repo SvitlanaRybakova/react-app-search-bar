fac320700f369832612f26ad56c89dcacbac18cf
Svitlana <rsv0011@gmail.com>
2021-02-17 17:48:47 +0100

    add styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Search from '../components/Search';
+import { body, container, articleWrapper } from '../styles/Home.module.css';
 
 function Home(){
   return(
-    <Search />
+    <>
+    <div className={container}>
+      <Search />
+      <div className={articleWrapper}></div>
+    </div>
+    
+    </>
+    
   )
 }
 export default Home;
