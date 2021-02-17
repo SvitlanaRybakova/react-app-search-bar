7dd5f44efdaae5d3cb472a97b216f75c38c66177
Svitlana <rsv0011@gmail.com>
2021-02-17 17:57:16 +0100

    create article component

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Article(){
+  return(
+    <>
+    <div className="articleItem">
+      <p className="title"></p>
+      <p className="content"></p>
+      <span className="date"></span>
+      <span className="author"></span>
+    </div>
+    </>
+  )
+}
+export default Article;
